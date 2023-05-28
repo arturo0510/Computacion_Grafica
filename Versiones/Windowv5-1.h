2daed5ce1650ca2f6a73bb4563cea8c143d52aef
FrancisBL5 <98990804+FrancisBL5@users.noreply.github.com>
2023-05-28 14:20:05 -0600

    Versión 5.1

---
+++ Versiones/Windowv5-1.h
@@ -0,0 +1,58 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
+
+	GLfloat getapagalinterna() { return apagalinterna; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);
+	}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+
+	// Metodos para obtener valores de banderas
+	bool getabrirBaul() { return abrirBaul; }
+	void setabriBaul(bool abrir) { abrirBaul = abrir; }
+
+	bool getFaro1() { return faro1; }
+	bool getFaro2() { return faro2; }
+
+	~Window();
+private:
+	GLFWwindow* mainWindow;
+	GLint width, height;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat muevex;
+	GLfloat muevexh, muevezh;
+	GLfloat movcoche, movcochelin, rotazh,rotllantas;
+	GLfloat apagalinterna = 1;
+	GLfloat banderaposicion,tirar_d,reiniciar_d;
+	bool mouseFirstMoved;
+	
+	// Banderas por teclado
+	bool abrirBaul = false;
+	bool faro1 = false;
+	bool faro2 = false;
+
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
